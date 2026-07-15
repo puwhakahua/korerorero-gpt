c482f1dcf20544b667f86a89012b34f71867234c
kjdon <kjdon@community.nzdl.org>
2026-07-15 17:56:35 +1200

    some changes for when its run as part of running pm2 as a service

--- SETUP.sh
+++ SETUP.sh
@@ -1,13 +1,25 @@
-export NVM_DIR=`pwd`/nvm
-echo "Set NVM_DIR = $NVM_DIR"
+#!/usr/bin/env bash
 
-echo "Sourcing nvm.sh"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export NVM_DIR="$SCRIPT_DIR/nvm"
+
+# this only outputs if stdout is connected to a terminal - true for running manually, false
+# for when run by systemd
+if [[ -t 1 ]]; then
+    echo "Set NVM_DIR = $NVM_DIR"
+    echo "Sourcing nvm.sh"
+fi
+
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-echo "nvm: $(command -v nvm)"
 
-echo "Changing version of NodeJS used to v20"
-nvm use 20
+if [[ -t 1 ]]; then
+    echo "nvm: $(command -v nvm)"
+    echo "Changing version of NodeJS used to v20"
+fi
+nvm use default >/dev/null
 
-echo "node: $(which node)"
-
-echo "pm2: $(which pm2)"
+if [[ -t 1 ]]; then
+    echo "node: $(which node)"
+    echo "pm2: $(which pm2)"
+fi
